5ad47508207916ab1bfd5d378246d17e8e04659b
Abhishek kochar <abhishekkochar2@Gmail.com>
2022-07-14 19:54:14 +1200

    Uniswap Fork. Fixed couple of bugs when building.

---
+++ src/index.tsx
@@ -0,0 +1,70 @@
+import '@reach/dialog/styles.css'
+import 'inter-ui'
+import 'polyfills'
+import 'components/analytics'
+
+import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
+import { MulticallUpdater } from 'lib/state/multicall'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+
+import Blocklist from './components/Blocklist'
+import Web3Provider from './components/Web3Provider'
+import { LanguageProvider } from './i18n'
+import App from './pages/App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import store from './state'
+import ApplicationUpdater from './state/application/updater'
+import ListsUpdater from './state/lists/updater'
+import LogsUpdater from './state/logs/updater'
+import TransactionUpdater from './state/transactions/updater'
+import UserUpdater from './state/user/updater'
+import ThemeProvider, { ThemedGlobalStyle } from './theme'
+import RadialGradientByChainUpdater from './theme/RadialGradientByChainUpdater'
+
+if (!!window.ethereum) {
+  window.ethereum.autoRefreshOnNetworkChange = false
+}
+
+function Updaters() {
+  return (
+    <>
+      <RadialGradientByChainUpdater />
+      <ListsUpdater />
+      <UserUpdater />
+      <ApplicationUpdater />
+      <TransactionUpdater />
+      <MulticallUpdater />
+      <LogsUpdater />
+    </>
+  )
+}
+
+ReactDOM.render(
+  <StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <LanguageProvider>
+          <Web3Provider>
+            <Blocklist>
+              <BlockNumberProvider>
+                <Updaters />
+                <ThemeProvider>
+                  <ThemedGlobalStyle />
+                  <App />
+                </ThemeProvider>
+              </BlockNumberProvider>
+            </Blocklist>
+          </Web3Provider>
+        </LanguageProvider>
+      </HashRouter>
+    </Provider>
+  </StrictMode>,
+  document.getElementById('root')
+)
+
+if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
+  serviceWorkerRegistration.register()
+}
---
+++ src/setupTests.ts
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom' // jest custom assertions
+
+import { Readable } from 'stream'
+import { TextDecoder, TextEncoder } from 'util'
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.ReadableStream = Readable as unknown as typeof globalThis.ReadableStream
+  global.TextEncoder = TextEncoder
+  global.TextDecoder = TextDecoder as typeof global.TextDecoder
+}
