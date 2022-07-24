f3437ff4269130a52c5796ef2e032fff5ab26538
Jia Jian Goi <jiajian@cakedefi.com>
2022-07-25 02:14:46 +0800

    chore(ga): updated deprecated ga functions

--- src/components/analytics/GoogleAnalyticsReporter.tsx
+++ src/components/analytics/GoogleAnalyticsReporter.tsx
@@ -5,7 +5,7 @@ import { RouteComponentProps } from 'react-router-dom'
 // fires a GA pageview every time the route changes
 export default function GoogleAnalyticsReporter({ location: { pathname, search } }: RouteComponentProps): null {
   useEffect(() => {
-    ReactGA.pageview(`${pathname}${search}`)
+    ReactGA.send({ hitType: 'pageview', page: `${pathname}${search}` })
   }, [pathname, search])
   return null
 }
