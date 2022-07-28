152a99650e77e6a31a46ec77bad0bef92af5262a
chee-chyuan <angchee@live.com.my>
2022-07-28 12:53:48 +0800

    modify 'your liquidity' not done

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,11 +1,10 @@
 import React, { useContext, useMemo } from 'react'
 import styled, { ThemeContext } from 'styled-components'
-import { Pair, JSBI } from '@uniswap/sdk'
+import { Pair } from '@uniswap/sdk'
 import { Link } from 'react-router-dom'
 import { SwapPoolTabs } from '../../components/NavigationTabs'
 import FullPositionCard from '../../components/PositionCard'
-//import { useUserHasLiquidityInAllTokens } from '../../data/V1'
-import { useTokenBalancesWithLoadingIndicator } from '../../state/wallet/hooks'
+import { useGetProxyLiquidityOfUser, useProxies } from '../../state/wallet/hooks'
 import { TYPE, HideSmall } from '../../theme'
 import { Text } from 'rebass'
 import Card from '../../components/Card'
@@ -15,11 +14,8 @@ import { AutoColumn } from '../../components/Column'
 
 import { useActiveWeb3React } from '../../hooks'
 import { usePairs } from '../../data/Reserves'
-import { toV2LiquidityToken, useTrackedTokenPairs } from '../../state/user/hooks'
 import { Dots } from '../../components/swap/styleds'
 import { CardSection, DataCard, CardNoise, CardBGImage } from '../../components/earn/styled'
-import { useStakingInfo } from '../../state/stake/hooks'
-import { BIG_INT_ZERO } from '../../constants'
 
 const PageWrapper = styled(AutoColumn)`
   max-width: 640px;
@@ -78,49 +74,64 @@ export default function Pool() {
   const { account } = useActiveWeb3React()
 
   // fetch the user's balances of all tracked V2 LP tokens
-  const trackedTokenPairs = useTrackedTokenPairs()
-  const tokenPairsWithLiquidityTokens = useMemo(
-    () => trackedTokenPairs.map(tokens => ({ liquidityToken: toV2LiquidityToken(tokens), tokens })),
-    [trackedTokenPairs]
-  )
-  const liquidityTokens = useMemo(() => tokenPairsWithLiquidityTokens.map(tpwlt => tpwlt.liquidityToken), [
-    tokenPairsWithLiquidityTokens
-  ])
-  const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
-    account ?? undefined,
-    liquidityTokens
-  )
+  // const trackedTokenPairs = useTrackedTokenPairs()
+  // const tokenPairsWithLiquidityTokens = useMemo(
+  //   () => trackedTokenPairs.map(tokens => ({ liquidityToken: toV2LiquidityToken(tokens), tokens })),
+  //   [trackedTokenPairs]
+  // )
+  // const liquidityTokens = useMemo(() => tokenPairsWithLiquidityTokens.map(tpwlt => tpwlt.liquidityToken), [
+  //   tokenPairsWithLiquidityTokens
+  // ])
+  // const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
+  //   account ?? undefined,
+  //   liquidityTokens
+  // )
 
   // fetch the reserves for all V2 pools in which the user has a balance
-  const liquidityTokensWithBalances = useMemo(
-    () =>
-      tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
-        v2PairsBalances[liquidityToken.address]?.greaterThan('0')
-      ),
-    [tokenPairsWithLiquidityTokens, v2PairsBalances]
-  )
+  // const liquidityTokensWithBalances = useMemo(
+  //   () =>
+  //     tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
+  //       v2PairsBalances[liquidityToken.address]?.greaterThan('0')
+  //     ),
+  //   [tokenPairsWithLiquidityTokens, v2PairsBalances]
+  // )
 
-  const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens))
-  const v2IsLoading =
-    fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair)
+  // const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens))
+  // const v2IsLoading =
+  //   fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair)
 
-  const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
+  // const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
 
+  // // show liquidity even if its deposited in rewards contract
+  // const stakingInfo = useStakingInfo()
+  // const stakingInfosWithBalance = stakingInfo?.filter(pool => JSBI.greaterThan(pool.stakedAmount.raw, BIG_INT_ZERO))
+  // const stakingPairs = usePairs(stakingInfosWithBalance?.map(stakingInfo => stakingInfo.tokens))
 
-  // show liquidity even if its deposited in rewards contract
-  const stakingInfo = useStakingInfo()
-  const stakingInfosWithBalance = stakingInfo?.filter(pool => JSBI.greaterThan(pool.stakedAmount.raw, BIG_INT_ZERO))
-  const stakingPairs = usePairs(stakingInfosWithBalance?.map(stakingInfo => stakingInfo.tokens))
+  // // remove any pairs that also are included in pairs with stake in mining pool
+  // const v2PairsWithoutStakedAmount = allV2PairsWithLiquidity.filter(v2Pair => {
+  //   return (
+  //     stakingPairs
+  //       ?.map(stakingPair => stakingPair[1])
+  //       .filter(stakingPair => stakingPair?.liquidityToken.address === v2Pair.liquidityToken.address).length === 0
+  //   )
+  // })
 
-  // remove any pairs that also are included in pairs with stake in mining pool
-  const v2PairsWithoutStakedAmount = allV2PairsWithLiquidity.filter(v2Pair => {
-    return (
-      stakingPairs
-        ?.map(stakingPair => stakingPair[1])
-        .filter(stakingPair => stakingPair?.liquidityToken.address === v2Pair.liquidityToken.address).length === 0
-    )
-  })
+  const proxies = useProxies()
+  const [userProxyLiquidity, fetchingProxyLiquidity] = useGetProxyLiquidityOfUser(account ?? undefined, proxies)
+  const proxyWithBalance = useMemo(() => proxies.filter(p => userProxyLiquidity[p.address]?.greaterThan('0')), [
+    proxies,
+    userProxyLiquidity
+  ])
+  const proxyV2Pairs = usePairs(proxyWithBalance.map(p => [p.tokenA, p.tokenB]))
+  const proxyV2PairsWithLiquidity = proxyV2Pairs
+    .map(([, pair]) => pair)
+    .filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
 
+  const userProxyLiquidityIsLoading =
+    fetchingProxyLiquidity || proxyV2Pairs?.length < proxyWithBalance.length || proxyV2Pairs?.some(V2Pair => !V2Pair)
+
+  console.log(proxyV2PairsWithLiquidity)
+  console.log(userProxyLiquidityIsLoading)
   return (
     <>
       <PageWrapper>
@@ -184,6 +195,32 @@ export default function Pool() {
                   Connect to a wallet to view your liquidity.
                 </TYPE.body>
               </Card>
+            ) : userProxyLiquidityIsLoading ? (
+              <EmptyProposals>
+                <TYPE.body color={theme.text3} textAlign="center">
+                  <Dots>Loading</Dots>
+                </TYPE.body>
+              </EmptyProposals>
+            ) : proxyWithBalance?.length > 0 ? (
+              <>
+                {proxyV2PairsWithLiquidity.map(v2Pair => (
+                  <FullPositionCard key={v2Pair.liquidityToken.address} pair={v2Pair} />
+                ))}
+              </>
+            ) : (
+              <EmptyProposals>
+                <TYPE.body color={theme.text3} textAlign="center">
+                  No liquidity found.
+                </TYPE.body>
+              </EmptyProposals>
+            )}
+
+            {/* {!account ? (
+              <Card padding="40px">
+                <TYPE.body color={theme.text3} textAlign="center">
+                  Connect to a wallet to view your liquidity.
+                </TYPE.body>
+              </Card>
             ) : v2IsLoading ? (
               <EmptyProposals>
                 <TYPE.body color={theme.text3} textAlign="center">
@@ -212,7 +249,7 @@ export default function Pool() {
                   No liquidity found.
                 </TYPE.body>
               </EmptyProposals>
-            )}
+            )} */}
           </AutoColumn>
         </AutoColumn>
       </PageWrapper>
