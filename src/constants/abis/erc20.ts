550f89ea12a471562f85e148fb6d727d212d0a59
eatBlock <43106142+Abhishekkochar@users.noreply.github.com>
2022-08-01 17:06:59 +1200

    fix: removing LP functions (#25)
    * Some changes to test the Balance when removing LP
    
    * Fix the add/Remove LP UI. Enable the lp balance when removing LP based on user
    
    * Removed add UNI-V2 link. Balance when removing Lp working.
    
    * Working removing LP for DFI/ETH & DFI/WETH pair. Still working on other pairs
    
    * Working removing LP for all pairs.Removed approve function when removing LP
    
    * Added function to choose contract address based on chainID
    
    * Selected addresses based on chainId added
    
    * Fixed the PROXIES array and removed the array.
    
    * Fixed bug preventing showing 'YOURS LIQUIDITY' section
    
    (cherry picked from commit 256f43ea420c860d042a57498e8611e5d15e116e)

--- src/constants/abis/erc20.ts
+++ src/constants/abis/erc20.ts
@@ -1,10 +1,19 @@
 import { Interface } from '@ethersproject/abi'
 import ERC20_ABI from './erc20.json'
 import ERC20_BYTES32_ABI from './erc20_bytes32.json'
+import ETH_LP_ABI from './eth-lp-proxy.json'
+import USDC_LP_ABI from './usdc-lp-proxy.json'
+import USDT_LP_ABI from './usdt-lp-proxy.json'
 
 const ERC20_INTERFACE = new Interface(ERC20_ABI)
 
 const ERC20_BYTES32_INTERFACE = new Interface(ERC20_BYTES32_ABI)
 
+const ETH_LP_ABI_INTERFACE = new Interface(ETH_LP_ABI)
+
+const USDC_LP_ABI_INTERFACE = new Interface(USDC_LP_ABI)
+
+const USDT_LP_ABI_INTERFACE = new Interface(USDT_LP_ABI)
+
 export default ERC20_INTERFACE
-export { ERC20_ABI, ERC20_BYTES32_INTERFACE, ERC20_BYTES32_ABI }
+export { ERC20_ABI, ERC20_BYTES32_INTERFACE, ERC20_BYTES32_ABI, ETH_LP_ABI_INTERFACE, USDC_LP_ABI_INTERFACE, USDT_LP_ABI_INTERFACE }
