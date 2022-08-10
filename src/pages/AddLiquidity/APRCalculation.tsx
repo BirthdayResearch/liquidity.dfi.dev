f291d5ff96a706bc4a051f7bfa6efeab2a52da48
Abhishek <abhishekkochar2@gmail.com>
2022-08-10 17:33:56 +1200

    Fixed Over and under flow issue

--- src/pages/AddLiquidity/APRCalculation.tsx
+++ src/pages/AddLiquidity/APRCalculation.tsx
@@ -1,24 +1,19 @@
 import { JSBI, Pair } from "@uniswap/sdk";
 import { BigNumber } from '@ethersproject/bignumber'
 
-export async function apr( lpPair:Pair, totalSupply: BigNumber, rewardRate: BigNumber, totalStake: BigNumber){
-   //const re =  BigNumber.from(JSBI.toNumber(lpPair.reserveOf(lpPair.token0).raw))
-    //const rewardRate = 0.1
-    const rewardSpeed = rewardRate.toNumber()/10**lpPair.token0.decimals
-    const blocksPerWeek = 6000*7
-    const tokenAreserve = JSBI.divide(lpPair.reserveOf(lpPair.token0).raw, JSBI.BigInt(10**lpPair.token0.decimals))
-    const tokenBreserve = JSBI.divide(lpPair.reserveOf(lpPair.token1).raw, JSBI.BigInt(10**lpPair.token1.decimals))
-    const tokenAPrice = JSBI.toNumber(tokenBreserve)/ JSBI.toNumber(tokenAreserve)
-    const tokenBPrice = JSBI.divide(tokenAreserve, tokenBreserve)
+export async function apr( lpPair:Pair, totalSupplyUni: BigNumber, rewardRate: BigNumber, totalStake: BigNumber){
+
+    const blocksPerWeek = 6000 * 7
+    const tokenAreserve = lpPair.reserveOf(lpPair.token0).raw
+    const tokenBreserve = lpPair.reserveOf(lpPair.token1).raw
+    const tokenAPrice = JSBI.toNumber(tokenBreserve) / JSBI.toNumber(tokenAreserve)
     const tokenOneLiquidity = JSBI.toNumber(tokenAreserve) * tokenAPrice
-    const tokenTwoLiquidity = JSBI.multiply(tokenBreserve,  tokenBPrice)
-    const totalLp =  tokenOneLiquidity + JSBI.toNumber(tokenTwoLiquidity)
-    const totalSupplyUni = totalSupply.toNumber()/10**18
-    const lpTokenPrice = (totalLp/totalSupplyUni)
-    const totalStaked = totalStake.toNumber()/10**18
-    //const totalRewardPerDay = rewardSpeed * blocksPerDay
-    //const poolDailyRewardRate = totalRewardPerDay / totalStaked
-    
-    const apr = (blocksPerWeek * rewardSpeed * tokenAPrice *52) / ((totalLp / totalSupplyUni) * totalStaked )*100
-    return ['Token A Price: ', tokenAPrice, 'Token B Price: ', tokenBPrice, 'Token A Reserve: ',JSBI.toNumber(tokenAreserve), 'Token B Reserve: ' ,JSBI.toNumber(tokenBreserve), 'Token LP: ', totalLp, 'LP price: ', lpTokenPrice, "APR: ", apr, 'Reward Speed',rewardSpeed]
+    const tokenTwoLiquidity = tokenBreserve
+    const totalLp = tokenOneLiquidity + JSBI.toNumber(tokenTwoLiquidity)
+    const rewardSpeed = (rewardRate.mul(blocksPerWeek))
+    const rewardinTookenA = rewardSpeed.toNumber()*(tokenAPrice)
+    //totalSupplyinCirculation = totalLp/totalSupplyUni
+    const totalSupplyinCirculation = totalLp/JSBI.toNumber(JSBI.BigInt(totalSupplyUni.toString()))
+    const apr = (rewardinTookenA * 52)/(totalSupplyinCirculation * totalStake.toNumber()) *100
+    return ['Token A Price: ', tokenAPrice, 'Token A Reserve: ', JSBI.toNumber(tokenAreserve), 'Token B Reserve: ', JSBI.toNumber(tokenBreserve), 'Token LP: ', totalLp, "APR: ", apr, 'Reward Speed', rewardSpeed, totalSupplyinCirculation]
 }
