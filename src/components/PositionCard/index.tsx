e2e4596dc04d47000c4c1e79dff619999992ef07
Jia Jian Goi <jiajian@cakedefi.com>
2022-08-12 21:03:41 +0800

    fix(pool): fixed apr overflow (#105)

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { currencyEquals, JSBI, Pair, Percent, TokenAmount } from '@uniswap/sdk'
 import { BigNumber } from '@ethersproject/bignumber'
 import { darken } from 'polished'
@@ -35,7 +34,7 @@ import { RowBetween, RowFixed, AutoRow } from '../Row'
 import { Dots } from '../swap/styleds'
 import { BIG_INT_ZERO, USDC, USDT } from '../../constants'
 import { useClaimRewardProxyCallback } from 'hooks/useApproveCallback'
-// import { apr } from 'pages/AddLiquidity/APRCalculation'
+import { apr } from 'pages/AddLiquidity/APRCalculation'
 import { useTotalRewardsAccrued } from 'data/Rewards'
 
 export const FixedHeightRow = styled(RowBetween)`
@@ -283,9 +282,9 @@ export default function FullPositionCard({ pair, border, stakedBalance, claimabl
       return rewardRateETH ? rewardRateETH[1] : undefined
     }
   }
-  // let aprValue: number | undefined = 0
+  let aprValue: number | undefined = 0
   if (pair && totalSupply && checkRewardContract() && checkTotalStake()) {
-    // aprValue = apr(pair, totalSupply, checkRewardContract(), checkTotalStake())
+    aprValue = apr(pair, totalSupply, checkRewardContract(), checkTotalStake())
     //console.log(aprValue, chainId)
   }
 
@@ -300,7 +299,7 @@ export default function FullPositionCard({ pair, border, stakedBalance, claimabl
               {!currency0 || !currency1 ? <Dots>Loading</Dots> : `${currency0.symbol}/${currency1.symbol}`}
             </Text>
             <Text fontWeight={500} fontSize={14}>
-              <Badge>{/* {aprValue ? Math.round(aprValue) + '% APR' : ''} */}</Badge>
+              <Badge>{aprValue ? Math.round(aprValue) + '% APR' : ''}</Badge>
             </Text>
           </AutoRow>
           <RowFixed gap="8px">
@@ -334,14 +333,14 @@ export default function FullPositionCard({ pair, border, stakedBalance, claimabl
             </FixedHeightRow>
 
             <ContentCard>
-              {/* <FixedHeightRow>
+              <FixedHeightRow>
                 <Text fontSize={16} fontWeight={500}>
                   APR:
                 </Text>
                 <Text fontSize={16} fontWeight={500}>
                   {aprValue ? Math.round(aprValue) + '%' : ''}
                 </Text>
-              </FixedHeightRow> */}
+              </FixedHeightRow>
               <FixedHeightRow>
                 <Text fontSize={16} fontWeight={500}>
                   Rewards pool share:
