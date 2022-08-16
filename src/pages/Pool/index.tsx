183c4249d5599fac3468293110bfd05b052a6639
Abhishek <abhishekkochar2@gmail.com>
2022-08-16 17:14:10 +1200

    Fixed Elsint error

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -64,7 +64,7 @@ const EmptyProposals = styled.div`
 export default function Pool() {
   const theme = useContext(ThemeContext)
   const { account, chainId } = useActiveWeb3React()
-  
+
   const proxies = useProxies()
   const [userProxyLiquidity, fetchingProxyLiquidity] = useGetProxyLiquidityOfUser(account ?? undefined, proxies)
 
@@ -80,7 +80,6 @@ export default function Pool() {
     fetchingClaimable ||
     proxyV2Pairs2?.length < proxies.length ||
     proxyV2Pairs2?.some(V2Pair => !V2Pair)
-
 
   return (
     <>
