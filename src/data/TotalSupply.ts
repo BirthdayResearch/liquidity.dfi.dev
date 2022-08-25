6eb88ba4835780996e558f0cd01db63a0d6d7e1e
Abhishek <abhishekkochar2@gmail.com>
2022-08-25 19:30:53 +1200

    Fix the APR issue

--- src/data/TotalSupply.ts
+++ src/data/TotalSupply.ts
@@ -9,25 +9,6 @@ import {
   useUsdtLpContract
 } from '../hooks/useContract'
 import { useSingleCallResult } from '../state/multicall/hooks'
-//import { useBlockNumber } from 'state/application/hooks'
-//import { Contract } from 'ethers'
-
-// function rewardUpdate(contract: Contract) {
-//   return useSingleCallResult(contract, 'contract_stage')?.result?.[0]
-// }
-
-// function getColdStartBlockNum(contract: Contract) {
-//   return useSingleCallResult(contract, 'endColdStartBlockNum')?.result?.[0]
-// }
-// function calulateRewardrate(setRewardRate: BigNumber, currBlockNum: BigNumber, endColdStartBlockNum: BigNumber) {
-//   let rewardRate: BigNumber
-//   const EPOCH_LENGTH = BigNumber.from(6000 * 7)
-//   const currEpoch = currBlockNum.sub(endColdStartBlockNum).div(EPOCH_LENGTH)
-//   for (let i = 0; currEpoch.gt(i); i++) {
-//     rewardRate = setRewardRate.mul(96).div(100)
-//   }
-//   return rewardRate ? rewardRate : 'undefined'
-// }
 
 // returns undefined if input token is undefined, or fails to get token contract,
 // or contract total supply cannot be fetched
@@ -48,27 +29,27 @@ export function useTotalSupplyLP(address?: string) {
 }
 
 export function useUsdtRewardRate() {
-  const rewardRate: BigNumber = useSingleCallResult(useUsdtLpContract(), 'rewardSpeed')?.result?.[0]
+  const rewardRate: BigNumber = useSingleCallResult(useUsdtLpContract(), 'admin_speed')?.result?.[0]
   const totalStake: BigNumber = useSingleCallResult(useUsdtLpContract(), 'totalStake')?.result?.[0]
+  const contractStage: BigNumber = useSingleCallResult(useUsdtLpContract(), 'contract_stage')?.result?.[0]
 
-  return rewardRate && totalStake ? [rewardRate, totalStake] : undefined
+  return rewardRate && totalStake && contractStage ? [rewardRate, totalStake, contractStage] : undefined
 }
 
 export function useUsdcRewardRate() {
-  const rewardRate = useSingleCallResult(useUsdcLpContract(), 'rewardSpeed')?.result?.[0]
-  const totalStake = useSingleCallResult(useUsdcLpContract(), 'totalStake')?.result?.[0]
-  // const stage: number = useSingleCallResult(useUsdtLpContract(), 'contract_stage')?.result?.[0]
-  // const endColdStartBlockNum = useSingleCallResult(useUsdtLpContract(), 'endColdStartBlockNum')?.result?.[0]
-  // const EPOCH_LENGTH = 6000 * 7
-  // const adminRate = BigNumber.from(25 * 10 ** 6)
-  return rewardRate && totalStake ? [rewardRate, totalStake] : undefined
+  const rewardRate: BigNumber = useSingleCallResult(useUsdcLpContract(), 'admin_speed')?.result?.[0]
+  const totalStake: BigNumber = useSingleCallResult(useUsdcLpContract(), 'totalStake')?.result?.[0]
+  const contractStage: BigNumber = useSingleCallResult(useUsdcLpContract(), 'contract_stage')?.result?.[0]
+
+  return rewardRate && totalStake && contractStage ? [rewardRate, totalStake, contractStage] : undefined
 }
 
 export function useWethRewardRate() {
   const rewardRate: BigNumber = useSingleCallResult(useEthLpContract(), 'rewardSpeed')?.result?.[0]
   const totalStake: BigNumber = useSingleCallResult(useEthLpContract(), 'totalStake')?.result?.[0]
+  const contractStage: BigNumber = useSingleCallResult(useEthLpContract(), 'contract_stage')?.result?.[0]
 
-  return rewardRate && totalStake ? [rewardRate, totalStake] : undefined
+  return rewardRate && totalStake && contractStage ? [rewardRate, totalStake, contractStage] : undefined
 }
 
 export function useTotalStake(proxyAddress?: string, token?: Token): TokenAmount | undefined {
--- src/pages/AddLiquidity/APRCalculation.tsx
+++ src/pages/AddLiquidity/APRCalculation.tsx
@@ -5,26 +5,40 @@ import BN from 'bignumber.js'
 export const apr = (
   lpPair: Pair,
   totalSupplyUni: BigNumber,
-  rewardRate: BigNumber | undefined,
-  totalStake: BigNumber | undefined
+  adminRate: BigNumber | undefined,
+  totalStake: BigNumber | undefined,
+  contractStage: BigNumber | undefined,
+  currentBlockNum: BigNumber | undefined
 ): number | undefined => {
-  if (rewardRate === undefined || totalStake === undefined) return undefined
-  const rewardRateBN = new BN(rewardRate.toString())
+  let APR: number
+  const endColBlock = new BN(15389352)
+  if (
+    adminRate === undefined ||
+    totalStake === undefined ||
+    contractStage === undefined ||
+    currentBlockNum === undefined
+  )
+    return undefined
+  if (contractStage.toString() === '3') return (APR = 0)
+  let rewardRateBN = new BN(adminRate.toString())
   const totalStakeBN = new BN(totalStake.toString())
   const totalSupplyUniBN = new BN(totalSupplyUni.toString())
   const blocksPerWeek = 6000 * 7
+  const currEpoch = new BN(currentBlockNum.toString()).minus(endColBlock).div(blocksPerWeek)
+  for (let i = 0; currEpoch.gt(i); i++) {
+    rewardRateBN = rewardRateBN.times(96).div(100)
+  }
   const tokenAreserve = new BN(lpPair.reserveOf(lpPair.token0).raw.toString())
   const tokenBreserve = new BN(lpPair.reserveOf(lpPair.token1).raw.toString())
   const tokenAPrice = tokenBreserve.div(tokenAreserve)
-  // we consider the values of tokenBReserve and tokenAreserve are the same
   const totalLp = tokenBreserve.times(2)
   const rewardSpeed = rewardRateBN.times(blocksPerWeek)
-
   const rewardinTokenA = rewardSpeed.times(tokenAPrice)
   const uniTokenPrice = totalLp.div(totalSupplyUniBN)
-  const APR = rewardinTokenA
+  APR = rewardinTokenA
     .times(52)
     .div(uniTokenPrice.times(totalStakeBN))
     .times(100)
-  return APR.toNumber()
+    .toNumber()
+  return APR
 }
